82cb8343a5b7b6317ab81ea253574fa066662b37
shjin <shinpoongch1@gmail.com>
2026-07-07 17:28:26 +0900

    객체지향 학습: 상속(Inheritance)실습 곱셈,나눗셈

---
+++ PT2.java
@@ -0,0 +1,53 @@
+/*[요구사항]
+
+1. 부모 클래스(Calculator):
+int a, int b라는 필드를 가집니다. (계산할 숫자 두 개)
+void calculate() 메서드를 가집니다. (기본적으로 "두 수를 더합니다: " 라고 출력)
+2. 자식 클래스(MultiplyCalc, DivideCalc):
+calculator를 상속받습니다.
+calculate() 메서드를 오버라이딩하여 각각 곱하기 결과와 나누기 결과를 출력하도록 만드세요.
+
+3. 실행 클래스(MathSystem):
+MultiplyCalc 객체와 DivideCalc 객체를 생성합니다.
+두 객체에 각각 숫자를 넣어주고, calculate()를 호출하여 연산 결과를 확인하세요.*/
+
+
+class Calculator{
+    int a = 0;
+    int b = 0;
+    int c = 0;
+    void calculate() {
+        System.out.println("두 수를 더합니다");
+    }
+}
+
+
+class MultiplyCa extends Calculator{
+    void calculate() {
+        c = a * b;
+        System.out.println(c);
+    }
+}
+
+class  DivideCalc extends Calculator{
+    void calculate( ) {
+        c = a / b;
+        System.out.println(c);
+    }
+}
+    
+public class PT2{
+    public static void main(String[] args) {
+        MultiplyCa mymulti = new MultiplyCa();
+        DivideCalc mydivide = new DivideCalc();
+
+        mymulti.a = 10;
+        mymulti.b = 5; 
+        mymulti.calculate();
+
+        mydivide.a = 10;
+        mydivide.b = 5; 
+        mydivide.calculate();
+    
+    }
+}

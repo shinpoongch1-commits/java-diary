1e0759efc0422108bcd39d52b073ec42931661ae
shjin <shinpoongch1@gmail.com>
2026-07-09 03:04:13 +0900

    default 출력

--- DefaultStudy.java
+++ DefaultStudy.java
@@ -1,21 +1,33 @@
-/*orderService에 + Exception Error msg 출력' 
-public void pay() {
-    if (amount <= 0) {
-        throw new IllegalArgumentException("결제 금액은 0보다 커야 합니다!");
+/*결제 시스템에 "결제 일시를 출력하는 기능"을 모든 결제 수단에 공통으로 넣고 싶다고 해보죠.
+이제 NaverPay나 Toss는 printLog()를 새로 만들지 않아도, Payable 인터페이스를 구현했다는 이유만으로 printLog() 기능을 즉시 사용할 수 있습니다.
+// 인터페이스에 추가
+interface Payable {
+    void pay();
+    
+    default void printLog() {
+        System.out.println("로그: 결제 수단이 호출되었습니다.");
     }
-    System.out.println("결제 진행: " + amount + "원");
+}
 
+// 클래스들은 수정할 필요가 없음!
+// 하지만 원한다면 오버라이딩(재정의)도 가능함.
 
-    try {
-                p.pay();
-            } catch (Exception e) {
-                System.out.println("실패: " + e.getMessage());
-            }
+public class OrderService {
+    public static void main(String[] args) {
+        Payable p = new NaverPay(10000);
+        p.pay();
+        p.printLog(); // 인터페이스의 default 메서드를 호출!
+    }
+}
 }*/
 
 
 interface Payable{
     void pay();
+    
+    default void printLog() {
+        System.out.println("로그: 결제 수단이 호출되었습니다.");
+}
 }
 
 interface Refundable{
@@ -54,19 +66,21 @@ class Toss implements Payable, Refundable { // 'implements' 키워드 사용
 }
 }
 
-public class ExceptStudy{
+public class DefaultStudy{
     public static void main(String[] args){
 
-        Payable[] payments = {new NaverPay(0), new Toss(0)};
+        Payable[] payments = {new NaverPay(0), new Toss(20)};
 
         for (Payable p : payments) {
-            try { p.pay();}
-               catch (IllegalArgumentException e) {
+            try { p.pay();
+                  p.printLog(); }// 인터페이스의 default 메서드를 호출!}
+            catch (IllegalArgumentException e) {
                 System.out.println("오류발생 :" + e.getMessage());
                } 
             
-        }
+        
         
     }
 }
 
+}
